618ceada4556ebf476d3e75acdf42225b6666cc6
soenke kracht <sk@nrpl.com>
2015-04-29 10:15:34 +0200

    truth_tables fertig

--- Aufgabe3/truth_tables.rb
+++ Aufgabe3/truth_tables.rb
@@ -2,17 +2,34 @@ def truth_tables
   for zeile in 0 .. 3
     for spalte in 0 .. 3
       if zeile < 2 && spalte < 2
-      # erster Quadrant = AND
-        print 'a'
+        # erster Quadrant = AND
+        #print 'a'
+        if zeile % 2 == 1 && spalte % 2 == 1
+          print '1'
+        else
+          print '0'
+        end
       elsif zeile < 2 && spalte > 1
-      # zweiter Quadrant = OR
-        print 'b'
+        # zweiter Quadrant = OR
+        if zeile % 2 == 1 || spalte % 2 == 1
+          print '1'
+        else
+          print '0'
+        end
       elsif zeile > 1 && spalte < 2
-      # dritter Quadrant = NAND
-        print 'c'
+        # dritter Quadrant = NAND
+        if !(zeile % 2 == 1 && spalte % 2 == 1)
+          print '1'
+        else
+          print '0'
+        end
       else
-      # vierter Quadrant = NOR
-        print 'd'
+        # vierter Quadrant = NOR
+        if !(zeile % 2 == 1 || spalte % 2 == 1)
+          print '1'
+        else
+          print '0'
+        end
       end
     end
     puts ''
