38be640c74afe5348366e1fc2d48fdd1c97edf48
soenke kracht <sk@nrpl.com>
2015-06-10 12:47:02 +0200

    erste zwei Funktionen

--- Aufgabe6/A6 SoSe2015 Rekursion/a5-A/a5-A.rb
+++ Aufgabe6/A6 SoSe2015 Rekursion/a5-A/a5-A.rb
@@ -1,8 +1,24 @@
 def f(n)
+  if n == 1
+    return 1.0 / 6
+  else
+    return (1.0 / (n * (n + 1.0) * (n + 2.0))) + f(n - 1)
+  end
 end
 
-def ln(x,n)
+def ln(x, n)
+  if n == 0 && x > 0
+    return 2.0 * (x - 1.0) / (x + 1.0)
+  else
+    return (2.0 * ((x - 1.0) ** (2.0 * n + 1.0)) /
+    (((2.0 * n + 1.0) * ((x + 1.0) ** (2.0 * n + 1.0))))) +
+    ln(x,n-1)
+  end
 end
 
 def a(n,m)
 end
+
+#puts f(100)
+#puts Math::log(10)
+#puts ln(10,100)
--- Aufgabe6/A6 SoSe2015 Rekursion/a5-A/a5-Atesten.rb
+++ Aufgabe6/A6 SoSe2015 Rekursion/a5-A/a5-Atesten.rb
@@ -1,22 +1,36 @@
-require "a5-A/a5-A"
+require "A6 SoSe2015 Rekursion/a5-A/a5-A"
 
 def f_test()
+  abweichung = 1
+  zaehler = 0
+  while abweichung > 0.000001
+    zaehler += 1
+    abweichung = 0.25 - f(zaehler)
+  end
+  puts 'Die Abweichung betraegt ' + abweichung.to_s + ' im ' + zaehler.to_s + '. Durchlauf.'
 end
 
 f_test()
 
 def ln_test(x)
+  ln2 = Math::log(2)
+  abweichung = 1
+  zaehler = 0
+  while abweichung > 10**-15
+    zaehler += 1
+    abweichung = ln2 - ln(2, zaehler)
+  end
+  puts 'Die Abweichung betraegt ' + abweichung.to_s + ' im ' + zaehler.to_s + '. Durchlauf.'
+
 end
 
 ln_test(2)
 
-# 
+#
 def ack_test()
 end
 
 #ack_test3() Ackermann mit n=3
 def ack_test3()
 end
-
-
 
