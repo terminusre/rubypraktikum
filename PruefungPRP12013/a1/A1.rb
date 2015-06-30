70d9a56d449fc26e45f104826e99449c6e6d211b
soenke kracht <sk@nrpl.com>
2015-06-30 19:52:45 +0200

    prog

--- PruefungPRP12013/a1/A1.rb
+++ PruefungPRP12013/a1/A1.rb
@@ -2,45 +2,96 @@
 # Summe 5 PKt
 
 def f_1_4tel_it(n)
-
+  if !n.integer? || n < 1
+    return false
+  end
+  summe = 0
+  for i in 1 .. n
+    summe += 1.0 / (i * (i + 1.0) * (i + 2.0))
+  end
+  return summe
 end
 
 # TODO Naeherung fuer 1/4 rekursiv
 # Summe 5 Pkt
 def f_1_4tel_rek(n)
+  if !n.integer? || n < 1
+    return false
+  end
 
+  def end_rek(i, s)
+    if i > 0
+      end_rek(i - 1, (1.0 / (i * (i + 1.0) * (i + 2.0))) + s)
+    else
+      return s
+    end
+  end
+  return end_rek(n, 0)
 end
 
 # TODO Array erweitern ary_min_e_elems
 # Summe 10 Pkt
 class  Array
+  @schlecht = 0
+  def ary_min_2_elems_rec(counter)
+    if self.length > 0
+      if self[0].is_a?(Array)
+        counter += self[0].ary_min_2_elems_rec(counter)
+      end
+      if self.length > 1
+        @schlecht += 1
+        self[1 .. -1].ary_min_2_elems_rec(counter + 1)
+      end
+      return @schlecht
+    else
+      return @schlechts
+    end
+  end
+
   def ary_min_2_elems
-    return false
+    return ary_min_2_elems_rec(0)
   end
 end
+
+t = [1,[2,[3,4],[[7,[8,9]]]]]
+#print t.ary_min_2_elems
 
 # TODO  Methode organisiere_nach_wert(a_hash) 6 Pkt
 def organisiere_nach_wert(a_hash)
   other_hash = Hash.new
-  
+
   a_hash.values.map{|set| set.to_a}.flatten.each { |new_key|
     if !other_hash.key?(new_key)
       other_hash.store(new_key, Set.new)
     end
   }
-  
+
   a_hash.each {|pair|
     pair[1].each { |value|
       other_hash[value].add(pair[0])
     }
   }
-  
+
   return other_hash
 end
 
 require "Set"
 
 # TODO Methode schluessel_pro_wert(h) 4 Pkt
-def schluessel_pro_wert(h)
+def schluessel_pro_wert(a_hash)
+  other_hash = Hash.new
 
+  a_hash.values.map{|set| set.to_a}.flatten.each { |new_key|
+    if !other_hash.key?(new_key)
+      other_hash.store(new_key, 0)
+    end
+  }
+
+  a_hash.each {|pair|
+    pair[1].each { |value|
+      other_hash[value] += 1
+    }
+  }
+
+  return other_hash
 end
