c021ae716f82efd0d195eb6fbc204908ccaff8bd
soenke kracht <sk@nrpl.com>
2015-07-11 14:14:53 +0200

    ...

--- PruefungPRP12012/a1/A1.rb
+++ PruefungPRP12012/a1/A1.rb
@@ -1,6 +1,6 @@
 # TODO Naeherung fuer ln(x) iterativ
 # Summe 5 PKt
-def ln_iter(x,n) 
+def ln_iter(x,n)
 
 end
 
@@ -15,3 +15,26 @@ end
 
 # TODO Hash verknuepfen und auf injektiv pruefen
 # Summe 10
+class Hash
+  def verknuepfe(other)
+    new_hash = Hash.new
+    self.each { |pair|
+      if other.key?(pair[1])
+        new_hash[pair[0]] = other[pair[1]]
+      end
+    }
+    return new_hash
+  end
+
+  def injektiv?
+    a = Hash.new
+    self.values.each { |val| a[val] = 0 }
+
+    self.each { |pair|
+      if a.key?(pair[1])
+        a[pair[1]] += 1
+      end
+    }
+    print a
+  end
+end
